981a7bab357387e4d7fdc261c76aa048fc7fde3a
lucas <lucasprint@163.com>
2023-05-25 15:59:50 +0800

    完成爬虫

--- fils.py
+++ fils.py
@@ -10,3 +10,7 @@ def bulk_download(url_list:list,format:str,save_path:str):
             print("download %s done"%ele)
         else:
             print("%s is not a valid link for resource %s"%(ele,format))
+[import requests](poe://www.poe.com/_api/key_phrase?phrase=import%20requests&prompt=Tell%20me%20more%20about%20import%20requests.)
+files = {"file": open("file.txt", "rb")}
+response = requests.post(url, files=files)
+print(response.text)
--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import requests
-import json
 import re
-url = 'https://www.1ppt.com/article/106164.html#xiazai'
+import json
+url = 'https://www.ypppt.com/p/d.php?aid=2768'
 headers = {
-     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.55',
-     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-     'Accept-Encoding':'gzip, deflate, br',
-     'Accept-Language':'zh-CN,zh;q=0.9'
-}
-ppt = requests.get(url=url,headers=headers)
-# print(ppt.content.decode('utf-8'))
-print(ppt.text.encode('utf-8'))
-# print(re.findall(r'<title>(.*?)</title>',ppt.content.decode('utf-8')))
-# print(ppt.content.decode('utf-8'))
-# print(ppt.text.find('title'))
+          'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.55',
+          'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+          'Accept-Encoding':'gzip, deflate, br',
+          'Accept-Language':'zh-CN,zh;q=0.9'
+     }
+url2 = requests.get(url=url,headers=headers)
+url2.encoding = 'utf-8'
+down_url = re.findall(r'https://down\.ypppt\.com/uploads.*?\.zip',url2.text)
+if len(down_url) == 0:
+     down_url = re.findall(r'https://down\.ypppt\.com/uploads.*?\.rar',url2.text)
+
+print(down_url[0])
